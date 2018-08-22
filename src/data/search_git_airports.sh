a13a4ca0db925803fd9cdd3d0da483cf1ef40fa2
Abhi Sinha <sinha45@purdue.edu>
2018-08-22 19:06:40 -0400

    Attempted to use new dataset for airport coordinates

--- src/data/search_git_airports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Search data file from GitHub
-> /git_data/missing_airports.txt
-echo Searching for missing airports from github database...
-cat airport_names.txt | cut -f 2 -d ',' | while read x
-do
-	line=`grep "$x" 
