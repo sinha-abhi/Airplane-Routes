0a30249697dc4f7d3e00678b76f3139fb459cb15
Abhinav Sinha <abhinav.edu2017@gmail.com>
2019-01-10 14:46:47 -0500

    trivial changes

--- src/Hypergraph.py
+++ src/Hypergraph.py
@@ -1,30 +1,34 @@
 # Creates a hypergraph representation of the airplane data using information from acquired data sets
-
 import csv
 import os
 
-# final hypergraph, stored as: {plane number: [airports...]}
-graph = {}
 
-# read data file
-data_file_path = "839251150_T_ONTIME.csv"
-with open(os.path.join("data", data_file_path)) as data_file:
-    reader = csv.reader(data_file, delimiter=',')
+def get_flights_hgraph():
+    # final hypergraph, stored as: {plane number: [airports...]}
+    graph = {}
 
-    airports = []
-    prev_tail = ""
-    for tail_num, org, dest, ignore in reader:
-        if (tail_num != prev_tail) and (len(airports) > 0):
-            graph[prev_tail] = airports
-            prev_tail = tail_num
-            airports = []
+    data_file_path = "839251150_T_ONTIME.csv"
+    with open(os.path.join("data", data_file_path)) as data_file:
+        reader = csv.reader(data_file, delimiter=',')
 
-        if (len(airports) == 0) or (airports[-1] != org):
-            airports.append(org)
-        airports.append(dest)
+        airports = []
+        prev_tail = ""
+        for tail_num, org, dest, ignore in reader:
+            if (tail_num != prev_tail) and (len(airports) > 0):
+                graph[prev_tail] = airports
+                prev_tail = tail_num
+                airports = []
 
-with open("flights.hgraph", "w") as hgraph:
-    for v in graph.values():
-        v_str = " ".join(v)
-        res = str(len(v)) + " " + v_str + "\n"
-        hgraph.write(res)
+            if (len(airports) == 0) or (airports[-1] != org):
+                airports.append(org)
+            airports.append(dest)
+
+    with open(os.path.join("hypergraph", "flights.hgraph"), "w+") as hgraph:
+        for v in graph.values():
+            v_str = " ".join(v)
+            res = str(len(v)) + " " + v_str + "\n"
+            hgraph.write(res)
+
+
+if __name__ == "__main__":
+    get_flights_hgraph()
