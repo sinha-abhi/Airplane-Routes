4ba2279bb241c323892ff98c5f692e6cf47fc6ca
Abhi Sinha <sinha45@purdue.edu>
2019-02-02 00:26:37 -0500

    added coordinates to flights.airports

--- hypergraph.py
+++ hypergraph.py
@@ -2,6 +2,25 @@
 import csv
 import os
 
+def clean_airport_coords(file_path, write=None):
+    airports = {}
+
+    with open(os.path.join(file_path, "AIRPORT_GPS_COORD.csv")) as gps_file:
+        reader = csv.reader(gps_file, delimiter = ',')
+        
+        for row in reader:
+            airports[row[1]] = (row[6], row[7])
+        
+    sorted_airports = dict((k, airports[k]) for k in sorted(airports.keys()))
+
+    if write is not None:
+        with open(os.path.join(file_path, "master_coords.csv"), "w+") as gps_file:
+            for k in sorted_airports.keys():
+                gps_file.write(k + "," + sorted_airports[k][0] + "," + sorted_airports[k][1] + "\n")
+
+    return sorted_airports
+
+
 def make_hgraph(graph_dir, dat_path):
     graph = {}
     airports = set()
@@ -35,8 +54,8 @@ def make_hgraph(graph_dir, dat_path):
             elif tails[0] == row[0]:
                 regs[tails[0]] = row[6]
                 tails.remove(tails[0])
-        #if tails:
-            #print(tails)
+        if tails:
+            print(tails)
     
     with open(os.path.join(graph_dir, "flights.ids"), "w+") as acfts:
         for k, v in regs.items():
@@ -48,6 +67,7 @@ def make_hgraph(graph_dir, dat_path):
             res = str(len(v)) + " " + v_str + "\n"
             hgraph.write(res)
     
+
     locs = {}
     airports = sorted(airports)
     with open(os.path.join(dat_path, "L_AIRPORT_ID.csv")) as seq_file:
@@ -57,12 +77,22 @@ def make_hgraph(graph_dir, dat_path):
             if not airports:
                 break
             elif airports[0] == seq:
-                locs[seq] = name 
-                airports.remove(seq)
+                locs[name] = seq
+                airports.pop(0)
+
+    port_names = locs.keys()
+
+    gps_data = clean_airport_coords(dat_path, True)
+    for k, v in gps_data.items():
+        if not port_names:
+            break
+        for name in port_names:
+            if name in k:
+                locs[name] = str(locs[name]) + " " + str(v)
 
     with open(os.path.join(graph_dir, "flights.airports"), "w+") as apts:
         for k, v in locs.items():
-            apts.write(k + " " + v + "\n")
+            apts.write(v + " " + k + "\n")
 
 if __name__ == "__main__":
     make_hgraph("Hypergraph", "data")
